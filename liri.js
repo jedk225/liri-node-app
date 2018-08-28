4a7a7b5d6ed2906797176cc0805bf1f4cd1117b2
jedi225 <jedtk@uci.edu>
2018-08-27 22:55:33 -0700

    Working OMDB

--- liri.js
+++ liri.js
@@ -10,3 +10,62 @@ var moment = require("moment")
 var dotenv = require("dotenv");
 var fs = require("fs");
 
+var command = process.argv[2];
+//console.log(command);
+var queryName = process.argv.slice(3).join("+");
+
+
+//concert-this
+var concertURL = "https://rest.bandsintown.com/artists/" + queryName + "/events?app_id=codingbootcamp";
+
+//movie-this
+var queryUrl = 'http://www.omdbapi.com/?t=' + queryName + '&y=&plot=short&apikey=trilogy'
+
+
+if (command === "concert-this") {
+    request(concertURL, function (error, response, body) {
+        if (error) {
+            console.log('error:', error); // Print the error if one occurred
+
+        } else if (response.statusCode === 200) {
+            console.log("----------------"); // Print break.
+            console.log('Artist:', JSON.parse(body)[0].lineup[0]); // Print the Venue.
+            console.log(""); // Print space.
+            console.log('Venue Name:', JSON.parse(body)[0].venue.name); // Print the Venue.
+            console.log(""); // Print space.
+            console.log('Venue Location:', JSON.parse(body)[0].venue.city + ", " + JSON.parse(body)[0].venue.region); // Print the location.
+            console.log(""); // Print space.
+            console.log('Venue Date:', JSON.parse(body)[0].datetime); // Print the Date.
+            console.log("----------------"); // Print break.
+
+        }
+
+    });
+}
+else if (command === "movie-this") {
+    request(queryUrl, function (error, response, body) {
+        if (error) {
+            console.log('error:', error); // Print the error if one occurred
+
+        } else if (response.statusCode === 200) {
+            console.log('Title:', JSON.parse(body).Title); // Print title.
+            console.log(""); // Print space.
+            console.log('Release Year:', JSON.parse(body).Year); // Print Year.
+            console.log(""); // Print space.
+            console.log('IMDB Rating:', JSON.parse(body).imdbRating); // Print IMDB Rating.
+            console.log(""); // Print space.
+            console.log('Rotten Tomatoes:', JSON.parse(body).Ratings[1].Value); // Print Rotten Tomatoes Score.
+            console.log(""); // Print space.
+            console.log('Country:', JSON.parse(body).Country); // Print Country.
+            console.log(""); // Print space.
+            console.log('Language:', JSON.parse(body).Language); // Print Language.
+            console.log(""); // Print space.
+            console.log('Plot:', JSON.parse(body).Plot); // Print Plot.
+            console.log(""); // Print space.
+            console.log('Actors:', JSON.parse(body).Actors); // Print Actors.
+            console.log("----------------"); // Print break.
+
+        }
+
+    });
+}
